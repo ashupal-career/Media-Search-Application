2bbbc689e87b956fb74f14a7d519c2cf0016ef3b
ashupal-career <ashuvatsal007@gmail.com>
2026-04-15 15:52:40 +0530

    Using to way binding

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, incrementByAmount } from './redux/features/counteSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [num,setNum] = useState(5)
+
+  const dispatch = useDispatch() 
+  const count = useSelector((state)=>state.counter.value) 
+
 
   return (
     <>
-    <h1>Hello Ashu..!</h1>
+    <h1>{count}</h1>
+    <button onClick={() => {dispatch(increment())}}>Increment</button>
+    <button onClick={() => {dispatch(decrement())}}>Decrement</button>
+    <input type="number" onChange={(e)=>{console.log(e);
+    }}/>
+    <button onClick={() => {dispatch(incrementByAmount(10))}}>Increment by Amount</button>
     </>
   )
 }
