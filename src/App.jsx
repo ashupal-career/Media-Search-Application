709f1b930270b568f6a1421b28aac71bba416cb1
ashupal-career <ashuvatsal007@gmail.com>
2026-04-17 00:09:33 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { fetchPhotos, fetchVideos } from './api/mediaApi'
+import { fetchGIF, fetchPhotos, fetchVideos } from './api/mediaApi'
 import './App.css'
 
 function App() {
 
   return (
     <div className='h-screen text-white w-full bg-gray-950'>
-      <button onClick={async ()=> {
+      <button className='bg-green-400 px-4 py-2 m-5' onClick={async ()=> {
         const data = await fetchPhotos('panda')
         console.log(data.results);
         }}>Get Photos</button>
 
-      <button onClick={async ()=> {
+      <button className='bg-green-400 px-4 py-2 m-5' onClick={async ()=> {
         const data = await fetchVideos('panda')
         console.log(data.videos);
         }}>Get Videos</button>
+
+      <button className='bg-green-400 px-4 py-2 m-5' onClick={async ()=> {
+        const data = await fetchGIF('panda')
+        console.log(data);
+        }}>Get GIF</button>
     </div>
   )
 }
--- src/api/mediaApi.js
+++ src/api/mediaApi.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
 const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
+const TENOR_KEY = import.meta.env.VITE_TENOR_KEY
 
 export async function fetchPhotos(query,page=1,per_page=20) {
     const res = await axios.get('https://api.unsplash.com/search/photos',{
@@ -16,6 +17,15 @@ export async function fetchVideos(query,per_page=20) {
     const res = await axios.get('https://api.pexels.com/v1/videos/search',{
         params: {query,per_page},
         headers: {Authorization:PEXELS_KEY}
+    })
+
+    return res.data
+}
+
+export async function fetchGIF(query,per_page=20) {
+    const res = await axios.get('https://tenor.googleapis.com/v2/search',{
+        params: {query,per_page},
+        headers: {Authorization:TENOR_KEY}
     })
 
     return res.data
