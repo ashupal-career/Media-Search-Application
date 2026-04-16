74a303f08a5b70b6abec3df19e21af1bc0ff2c3a
ashupal-career <ashuvatsal007@gmail.com>
2026-04-16 21:38:21 +0530

    fix the error

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-import { useDispatch, useSelector } from 'react-redux'
-import { decrement, increment, incrementByAmount } from './redux/features/counteSlice'
 
 function App() {
-
-  const [num,setNum] = useState(5)
-
-  const dispatch = useDispatch() 
-  const count = useSelector((state)=>state.counter.value) 
-
-
   return (
-    <>
-    <h1>{count}</h1>
-    <button onClick={() => {dispatch(increment())}}>Increment</button>
-    <button onClick={() => {dispatch(decrement())}}>Decrement</button>
-    <input type="number" onChange={(e)=>{setNum(e.target.value);
-    }}/>
-    <button onClick={() => {dispatch(incrementByAmount(Number(num)))}}>Increment by Amount</button>
-    </>
+    <div className='h-screen text-white w-full bg-gray-950'>
+    <h1>I'm Ashu</h1>
+    </div>
   )
 }
 
