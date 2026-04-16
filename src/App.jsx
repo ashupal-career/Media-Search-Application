f6f75a23b85abf26a26bd8aa4797392acea6cae5
ashupal-career <ashuvatsal007@gmail.com>
2026-04-16 22:47:06 +0530

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { fetchPhotos } from './api/mediaApi'
 import './App.css'
 
 function App() {
+
   return (
     <div className='h-screen text-white w-full bg-gray-950'>
-    <h1>I'm Ashu</h1>
+      <button onClick={()=> {fetchPhotos('cat')}}>Get Photos</button>
     </div>
   )
 }
