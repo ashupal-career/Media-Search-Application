68efb3a20e620a76bba7a237d9ae0e384a50c982
ashupal-career <ashuvatsal007@gmail.com>
2026-04-18 23:42:36 +0530

    Add all tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import SearchBar from './components/SearchBar'
 import './App.css'
+import Tabs from './components/Tabs'
 
 function App() {
 
   return (
     <div className='h-screen text-white w-full bg-gray-950'>
       <SearchBar/>
+      <Tabs/>
     </div>
   )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,9 +8,9 @@ const SearchBar = () => {
 
     const dispatch = useDispatch()
 
-    const submitHandler = (e)=>{
+    const submitHandler = (e) => {
         e.preventDefault()
-        dispatchEvent(setQuery())
+        dispatch(setQuery(text))   
         setText('')
     }
 
@@ -19,6 +19,7 @@ const SearchBar = () => {
         <form 
         onSubmit={(e)=>{submitHandler(e)}}
         className='flex  bg-gray-900 gap-5 p-10'>
+
             <input 
             value={text}
             onChange={(e)=>{setText(e.target.value);}}
@@ -26,6 +27,7 @@ const SearchBar = () => {
             className='w-full border-2 px-4 py-2 text-xl rounded outline-none'
             type="text" 
             placeholder='Search anything....!'/>
+
             <button className='active:scale-95 cursor-pointer border-2 px-4 py-2 text-xl rounded outline-none'>Search</button>
         </form>
     </div>
