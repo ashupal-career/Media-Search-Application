d3d918a13827be4766d632ebded2caf7ddbd0c1e
ashupal-career <ashuvatsal007@gmail.com>
2026-04-18 18:40:01 +0530

    Add Searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import { fetchGIF, fetchPhotos, fetchVideos } from './api/mediaApi'
+import SearchBar from './components/SearchBar'
 import './App.css'
 
 function App() {
 
   return (
     <div className='h-screen text-white w-full bg-gray-950'>
-      <button className='bg-green-400 px-4 py-2 m-5' onClick={async ()=> {
-        const data = await fetchPhotos('panda')
-        console.log(data.results);
-        }}>Get Photos</button>
-
-      <button className='bg-green-400 px-4 py-2 m-5' onClick={async ()=> {
-        const data = await fetchVideos('panda')
-        console.log(data.videos);
-        }}>Get Videos</button>
-
-      <button className='bg-green-400 px-4 py-2 m-5' onClick={async ()=> {
-        const data = await fetchGIF('panda')
-        console.log(data);
-        }}>Get GIF</button>
+      <SearchBar/>
     </div>
   )
 }
