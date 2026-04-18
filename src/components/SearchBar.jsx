45d8a4c0a1fea8d9b2191d6f4a60ca254d0e5f6d
ashupal-career <ashuvatsal007@gmail.com>
2026-04-18 19:00:40 +0530

    search update

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { setQuery } from '../redux/features/searchSlice'
 
 const SearchBar = () => {
 
     const [text, setText] = useState('')
 
+    const dispatch = useDispatch()
+
+    const submitHandler = (e)=>{
+        e.preventDefault()
+        dispatchEvent(setQuery())
+        setText('')
+    }
+
   return (
     <div>
-        <form className='flex  bg-gray-900 gap-5 p-10'>
+        <form 
+        onSubmit={(e)=>{submitHandler(e)}}
+        className='flex  bg-gray-900 gap-5 p-10'>
             <input 
+            value={text}
+            onChange={(e)=>{setText(e.target.value);}}
             required
             className='w-full border-2 px-4 py-2 text-xl rounded outline-none'
             type="text" 
