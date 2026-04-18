aa76188964885c485e6d2494afa8f48f16682823
ashupal-career <ashuvatsal007@gmail.com>
2026-04-18 18:19:41 +0530

    fix reducers

--- src/api/mediaApi.js
+++ src/api/mediaApi.js
@@ -21,11 +21,10 @@ export async function fetchVideos(query,per_page=20) {
 
     return res.data
 }
-
-export async function fetchGIF(query,per_page=20) {
+// 1:52:13
+export async function fetchGIF(query,limit=20) {
     const res = await axios.get('https://tenor.googleapis.com/v2/search',{
-        params: {query,per_page},
-        headers: {Authorization:TENOR_KEY}
+        params: {q:query,key:TENOR_KEY,limit}
     })
 
     return res.data
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { counterReducer } from "./reducer";
+import { searchReducer } from "./features/searchSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer
+    search: searchReducer,
   }
 });
