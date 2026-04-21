931eb78829477930f9383f1f6cac299345157f4e
ashupal-career <ashuvatsal007@gmail.com>
2026-04-22 02:01:19 +0530

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   jsx: 'react',
-  base: "/Media-Search-Application/"
+  base: "/"
 })
